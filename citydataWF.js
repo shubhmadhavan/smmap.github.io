26aeefac5695fcbcf3e72a2496f5fcbd0f9039e8
shubhmadhavan <61299593+shubhmadhavan@users.noreply.github.com>
2024-10-11 06:25:40 +0530

    Update citydataWF.js
    Update Information for all waterfalls, adding details such as State/District Name and other relevant facts

--- citydataWF.js
+++ citydataWF.js
@@ -1,33 +1,33 @@
  
-const csvWF = `Kempty Waterfall; ; ;easy;30.4879258;78.0371964;
-Sahastradhara; ; ;easy;30.3883919;78.1293615;
-Chuliya Waterfall; ; ;easy;24.9305361;75.5711968;on Chambal River, b/w Jawahar Sagar and Maharana Pratap Sagar
-Keoti Falls; ; ;easy;24.8164113;81.4529612;
-Chachai Waterfall; ; ;easy;24.7918077;81.30249;Sirmour
-Dhuandhar Waterfall; ; ;easy;23.1262738;79.8075411;on Narmada River
-Kapildhara Waterfall; ; ;easy;22.7008773;81.7055688;on Son River
-Rajrappa Waterfall; ; ;easy;23.6339889;85.7121331;on Damodar River
-Hundru Falls; ; ;easy;23.4508392;85.6667986;on Subarnarekha River
-Jonha Falls; ; ;easy;23.3423069;85.6102055;
-Barehipani Waterfall; ; ;easy;21.9328896;86.3801873;in Simplipal NP, 2-tiered waterfall, one of the highest waterfalls in India
-Elephant Falls; ; ;easy;25.5375374;91.8225513;Shillong
-Nohkalikai Falls; ; ;easy;25.2754203;91.6859562;tallest plunge waterfall in India
-Kynrem Falls; ; ;easy;25.2272466;91.7166567;
-Nohsngithiang or Mawsmai Falls; ; ;easy;25.2389306;91.732537;
-Tirathgarh Waterfall; ; ;easy;18.9139499;81.8651088;
-Duduma Waterfalls; ; ;easy;18.519303;82.4531033;
-Vajrai Waterfall; ; ;easy;17.7086116;73.8151002;
-Gokak Falls; ; ;easy;16.1878993;74.7775938;
-Dudhsagar Falls; ; ;easy;15.3144375;74.3143073;
-Jog Falls; ; ;easy;14.2293266;74.8138496;on Shravathi River downstream of Linganamakki Dam, upstream of Mahatma Gandhi HE Plant and Gersoppa Reservoir
-Kunchikal Falls; ; ;easy;13.6937514;75.0191385;
-Barkana Falls; ; ;easy;13.4881474;75.0935483;
-Shivanasamudra Falls; ; ;easy;12.2944508;77.1680322;
-Hogenakkal Water Falls; ; ;easy;12.1178537;77.7787566;
-Soochipara Waterfalls; ; ;easy;11.5115356;76.1637859;
-Vazhachal Waterfalls; ; ;easy;10.3014146;76.5925893;on Chalakudy River
-Athirappilly Water Falls; ; ;easy;10.2851072;76.5697643;on Chalakudy River
-Thalaiyar Falls; ; ;easy;10.2141832;77.6287186;
-Kallar Meenmutty Waterfalls; ; ;easy;8.7122176;77.1452266;
-
+const csvWF = `Kempty Falls;UK;UK;easy;30.4879258;78.0371964;Kempty Falls is in Tehri Garhwal dis, UK
+Sahasradhara;UK;UK;easy;30.3883919;78.1293615;Sahasradhara (Thousand Springs) Falls is in Dehradun, UK. It is on the banks of River Baldi (trib of Song, which is a trib of Sooswa, which is ultimately a trib of Ganga)
+Chuliya Falls;RS;RS;easy;24.9305361;75.5711968;Chuliya Falls is on Chambal River, just on Maharana Pratap Sagar. It lies downstream of  Jawahar Sagar
+Keoti Falls;MP;MP;easy;24.8164113;81.4529612;Keoti Falls lies in Rewa dis of MP. It is on Mahana river, a tributary of Tamsa or Tons River.
+Chachai Falls;MP;MP;easy;24.7918077;81.30249;Chachai Falls is located in Sirmour dis of MP. It is on Bihad River, a tributary of Tamsa or Tons River.
+Mukkha Falls;UP;UP;easy;24.69059;82.67732;Mukkha Falls is in Sonbhadra district of UP. It lies on the Belan River
+Dhuandhar Falls;MP;MP;easy;23.1262738;79.8075411;Dhuandhar Falls is on Narmada River. It is in Bhedaghat, Jabalpur dis, MP
+Kapildhara Falls;MP;MP;easy;22.7008773;81.7055688;Kapildhara Falls is on Son River. It lies in MP
+Rajrappa Falls;JH;JH;easy;23.6339889;85.7121331;Rajrappa Falls is on Damodar River. It is in Bokaro dis, JH
+Hundru Falls;JH;JH;easy;23.4508392;85.6667986;on Subarnarekha River, located just outside the city of Ranchi
+Jonha Falls;JH;JH;easy;23.3423069;85.6102055;also in Ranchi, on Raru River
+Barehipani Falls;OD;OD;easy;21.9328896;86.3801873;Barehipani Falls lies inside Simplipal NP, Mayurbhanj dis, OD. It is a 2-tiered Falls, one of the highest Falls in India
+Elephant Falls;MG;MG;easy;25.5375374;91.8225513;Elephant Falls is a two-tier Falls in Shillong, MG. Khasi name for it is Ka Kshaid Lai Pateng Khohsiew
+Nohkalikai Falls;MG;MG;easy;25.2754203;91.6859562;● tallest plunge Falls in India ● near Cherapunji, one of the wettest places on Earth ● below Nohkalikai falls is a plunge pool with water of an unusual shade of green
+Kynrem Falls;MG;MG;easy;25.2272466;91.7166567;● near Cherapunji in East Khasi dis, MG ● it is a 3-tiered Falls
+Nohsngithiang or Mawsmai Falls;MG;MG;easy;25.2389306;91.732537;It is a seven-segmented Falls (so it is also k/a Seven Sisters Falls). It is in East Khasi Hills dis, MG 
+Tirathgarh Falls;CG;CG;easy;18.9139499;81.8651088;● in Kanger Valley NP of CG ● a block type Falls on the Kanger River ● It is said to have been created in the aftermath of a lunar crater 
+Duduma Falls;OD;OD;easy;18.519303;82.4531033;Duduma Falls is in border of Koraput dis, OD. Machhakund HE Project is located near Duduma Falls.
+Vajrai Falls;MH;MH;easy;17.7086116;73.8151002;Bhambavli Vajrai Falls in Satara dis, MH is the 2nd highest plunge waterfall in India, after the Nohkalikai Falls in MG
+Gokak Falls;KN;KN;easy;16.1878993;74.7775938;Gokak Falls lies on Ghataprabha River in Belagavi dis, KN
+Dudhsagar Falls;GA;GA;easy;15.3144375;74.3143073;Dudhsagar Falls is a 4-tiered WF on Mandovi River, Goa. It is one of India's tallest WFs. It is inside the Mollem National Park.
+Jog Falls;KN;KN;easy;14.2293266;74.8138496;Jog Falls, aka Gersoppa Falls, lies on Shravathi River. It is located downstream of Linganamakki Dam and is upstream of both Mahatma Gandhi HE Plant and the Gersoppa Reservoir
+Kunchikal Falls;KN;KN;easy;13.6937514;75.0191385;Kunchikal Falls lies on Varahi/ Haladi River of KN (Varahi lies South of the Shravathi River. Tungabhadra River flows a bit West to Varahi)
+Barkana Falls;KN;KN;easy;13.4881474;75.0935483;● Barkana Falls is on the Seetha River ● It lies in Someshwara WLS and Agumbe Reserve Forest ● near Agumbe in Shimoga district
+Shivanasamudra Falls;KN;KN;easy;12.2944508;77.1680322;Shivanasamudra Falls lies on Kaveri River (part that lies in KN). The Bharachukki Falls and the Gaganchukki Falls together form the Shivanasamudra Falls. It lies upstream of Mekedatu Dam, Hogenakkal Falls and Mettur Dam (Stanley Res).
+Hogenakkal Falls;KN/TN;N/TN;easy;12.1178537;77.7787566;Hogenakkal Falls is on border of KN and TN. Carbonatite rocks in this site are considered to be the oldest of its kind in South Asia. It lies upstream of the Mettur Dam (Stanley Res of TN). 
+Soochipara Falls;KL;KL;easy;11.5115356;76.1637859;Soochipara Falls lies on the Chaliyar River of KL
+Vazhachal Falls;KL;KL;easy;10.3014146;76.5925893;Both Vazhachal Falls & Athirappilly Falls lie on Chalakudy River of KL
+Athirappilly Water Falls; ; ;easy;10.2851072;76.5697643;Both Vazhachal Falls & Athirappilly Falls lie on Chalakudy River of KL
+Thalaiyar Falls;TN;TN;easy;10.2141832;77.6287186;Thalaiyar Falls, aka Rat Tail Falls, is in Theni dis, TN. It is on Manjalar River, a trib of Vaigai River
+Kallar Meenmutty Falls;KL;KL;easy;8.7122176;77.1452266;Kallar Meenmutty Falls is in Thirunvanthapuram, KL
 `;

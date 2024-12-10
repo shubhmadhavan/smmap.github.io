3c57b6bf6ecad5f27dc7b0514ca2e3ba540c0199
shubhmadhavan <61299593+shubhmadhavan@users.noreply.github.com>
2024-12-10 20:31:55 +0530

    2024-12-10 20h31m
    - Map Data in Single Folder

---
+++ Map_Data/citydataDM.js
@@ -0,0 +1,85 @@
+const csvDM = `Kishanganga HE Project; ; ;easy; 34.6486313;74.7519341; on Neelum, trib of Jhelum
+Mangla Dam Lake; ; ;easy; 33.2113568;73.6509807; on Jhelum, lies on Jhelum River on India's border with KANGLA Pak
+Ratle Dam; ; ;easy; 33.1756706;75.785494; near Kishtwar on Chenab River
+Baglihar Dam; ; ;easy; 33.1545699;75.3511229; on Chenab River
+Ranjit Sagar Dam; ; ;easy; 32.4596104;75.7659211; on Ravi River, Mnemonic for the Sikh Sagars on Ravi, Beas, Satluj
+Shahpur Kandi Dam; ; ;easy; 32.3923972;75.6885592; on Ravi River
+Maharana Pratap Beas Sagar; ; ;easy; 31.9916099;76.0166561; next to Pong Dam Lake, Mnemonic for the Sikh Sagars on Ravi, Beas, Satluj
+Bhakra Dam; ; ;easy; 31.4164701;76.451946; on Satluj River, Bhakra dam forms the Gobind Sagar reservoir
+Gobind Sagar; ; ;easy; 31.4133005;76.4858919; on Satluj River, Mnemonic for the Sikh Sagars on Ravi, Beas, Satluj
+Kishau Dam (proposed); ; ;easy; 30.74980556;77.70443333; on Tons River (HP-UK border), KIT → Kishau, Ichari on Tons
+Ichari Dam; ; ;easy; 30.613611;77.791111; on Tons River (HP-UK border), KIT → Kishau, Ichari on Tons
+Lakhwar Dam; ; ;easy; 30.517983;77.948925; on Yamuna River, lies ENTIRELY in UK
+Hathni Kund barrage; ; ;easy; 30.3145471;77.583337; on Yamuna River, in Yamuna Nagar district
+Tehri Dam; ; ;easy; 30.4060548;78.4706599; on Bhagirathi River
+Chungthang Dam; ; ;easy; 27.5987441;88.6537239; on Teesta River
+Umiam/ Barapani Lake/ Dam; ; ;easy; 25.6654174;91.8928297; on Umiam River
+Bisalpur Reservoir; ; ;easy; 25.856263;75.3737003; at the meeting point of Khari and Banas River
+Khari Dam; ; ;easy; 25.705335;74.2753209; on Banas River (tributary of Chambal)
+Nandsamand Dam; ; ;easy; 24.9274606;73.7747077; on Banas River
+Jawahar Sagar Dam; ; ;easy; 25.0410762;75.6789803; upstream of Kota city on Chambal River
+Rana-Pratap Sagar; ; ;easy; 24.7900733;75.5490475; on Chambal River, in Chittorgarh district
+Gandhi Sagar; ; ;easy; 24.4536135;75.5143623; on Chambal River, in Madhya Pradesh
+Madhikeda Reservoir; ; ;easy; 25.5123334;77.8739264; on Sindh River
+Sukma Dugma Reservoir; ; ;easy; 25.1792106;78.5342696; on Betwa River
+Mata Tila Dam; ; ;easy; 24.681173;78.3347613; on Betwa River, Lalitpur district, Uttar Pradesh
+Rajghat Reservoir; ; ;easy; 24.681173;78.263272; on Betwa River, an inter-state dam project
+Swami Brahma Nand Dam; ; ;easy; 25.5498202;79.6823402; on Virma River
+Sujara Band; ; ;easy; 24.630049;79.1003199; on Dhasan River
+Govind Ballabh Pant Sagar; ; ;easy; 24.10856;82.8297062; on Rihand River, tributary of Son
+Indira Sagar Dam; ; ;easy; 22.2347231;76.6374354; on Narmada River
+Bargi Dam; ; ;easy; 22.8425525;79.9868265; on Narmada River
+Tawa Reservoir; ; ;easy; 22.4518184;77.9394447; on Tawa River, a tributary of Narmada
+Vallabh Sagar Dam; ; ;easy; 21.2540737;73.6638592; on Tapi River
+Massanjore Dam; ; ;easy; 24.1557136;87.284051; on Mayurakshi River, tributary of Hooghly
+Tilaiya Dam; ; ;easy; 24.3243035;85.5215529; on Barakar River
+Maithon Dam; ; ;easy; 23.8584476;86.8152079; on Barakar River
+Konar Reservoir; ; ;easy; 23.9444647;85.7516353; in Damodar-Barakar delta
+Tenughat Reservoir; ; ;easy; 23.7297787;85.7973052; on Damodar River
+Getalsud Dam; ; ;easy; 23.4554267;85.5346714; on Subarnarekha River
+Chandil Reservoir; ; ;easy; 23.0316844;86.0150167; on Subarnarekha River
+Mandira Dam; ; ;easy; 22.2713869;84.6528506; on Brahmani River
+Rengali Reservoir; ; ;easy; 21.3471019;84.9589402; on Brahmani River
+Hasdeo Reservoir; ; ;easy; 22.6670588;82.6507276; on Hasdeo River, a tributary of Mahanadi
+Hirakud Dam; ; ;easy; 21.6236917;83.7749867; on Mahanadi River
+Ganrel Reservoir; ; ;easy; 20.5918119;81.5384444; on Mahanadi River
+Dudhwana Dam; ; ;easy; 20.3086306;81.7745703; on Mahanadi River
+Balar Reservoir; ; ;easy; 21.5236469;82.498539; does not "lie" on a river, inside Barnawapara Forest Range
+Patora Dam; ; ;easy; 20.7162745;82.4557589; on Jonk River, tributary of Mahanadi
+Saundur Dam; ; ;easy; 20.2221791;82.1029349; on Pairi River
+Totladoh Dam; ; ;easy; 21.7068799;79.2317032; on Pench River, in Pench National Park
+Khadakpurna Dam; ; ;easy; 20.1145641;76.1156826; on Purna River
+Yedari Dam; ; ;easy; 19.76659;76.7163362; on Purna River
+Gangapur Bandh Sagar; ; ;easy; 20.0303617;73.6638592; on Godavari River
+Nath Sagar; ; ;easy; 19.5028712;75.2681086; on Godavari River
+Sriram Sagar Dam; ; ;easy; 18.9969425;78.2050213; on Godavari River, Ramagundam Floating Solar Plant
+Jolaput Reservoir; ; ;easy; 18.4418563;82.5572162; on Sileru River, tributary of Sabari
+Balimela Reservoir; ; ;easy; 18.2477451;82.2087787; on Sileru River, tributary of Sabari
+Ujani Dam; ; ;easy; 18.1364287;75.0961549; on Bhima River, tributary of Krishna
+Dhom Reservoir; ; ;easy; 17.4270624;74.7975191; on Krishna River
+Koyna Dam; ; ;easy; 17.0222578;73.75135; on Koyna River, tributary of Krishna
+Almatti Dam; ; ;easy; 16.1830157;75.90556; on Krishna River
+Prakasham Barrage; ; ;easy; 16.6753876;80.6311523; on Krishna River
+Nagarjuna Sagar; ; ;easy; 16.497667;79.0721551; on Krishna River
+Srisailam Dam; ; ;easy; 15.872536;78.285013; on Krishna River
+Kadam Dam; ; ;easy; 14.764963;77.820717; on Kadam River, tributary of Godavari
+Kandaleru Dam; ; ;easy; 14.3474;79.59405; on Pennar River
+Krishnagiri Dam; ; ;easy; 12.4973078; 78.1696715; on Ponnaiyar/ Thenpannai River
+Gorur Hemavati Reservoir; ; ;easy; 12.8206986; 76.0365749; on Hemavathi River
+Krishnaraja Sagara Reservoir; ; ;easy; 12.457095; 76.4967919; on Kaveri River, at meeting point of Kaveri, Hemavathi & Lakshmanteertha Rivers
+Mekedatu Dam; ; ;easy; 12.2576017; 77.4458151; on Kaveri River, located in Ramnagar district, Karnataka
+Mettur Dam Stanley Reservoir; ; ;easy; 11.9024462; 77.8334992; on Kaveri River, located in Tamil Nadu
+Bhavani Sagar Reservoir; ; ;easy; 11.4690605; 77.0787219; on Bhavani River, at meeting point of Bhavani and Moyar Rivers, Erode district, Tamil Nadu
+Singanallur; ; ;easy; 10.9880618; 77.0219106; on Noyyali River, a tributary of Cauvery River, Coimbatore, Tamil Nadu
+Alagapuri Dam; ; ;easy; 10.584302; 77.9690203; on Kodaganar River, a tributary of Amravati, itself a tributary of Kaveri
+Vankalakayam Dam; ; ;easy; 10.6629249; 76.8273807; on Bharathapuzha River
+Thoomurthy Dam; ; ;easy; 10.4837169; 77.1575358; on Bharathapuzha River
+Meenkara Dam; ; ;easy; 10.629581; 76.8022488; on Gayathri River, a tributary of Bharathapuzha River
+Aliyar Dam; ; ;easy; 10.4823747; 76.9671864; on Uppar River, a tributary of Bharathapuzha River
+Shoyalar Reservoir; ; ;easy; 10.3116351; 76.7686944; on Chalakudy River
+Idukki Dam; ; ;easy; 9.8429427; 76.9772021; on Periyar River, one of the largest dams in Kerala
+Mullaperiyar Dam; ; ;easy; 9.525965; 77.1491414; on Periyar River, located in Kerala but managed by Tamil Nadu
+Kallada Dam; ; ;easy; 8.9470509; 77.0857212; on Kallada River, located in Kerala
+Papanasam Lower Dam; ; ;easy; 8.6920237; 77.3590017; on Thamarabarani River, located in Tamil Nadu
+Karayar Dam; ; ;easy; 8.651334; 77.310444; on Thamarabarani River, located in Tamil Nadu
+`;
---
+++ Map_Data/citydataIN.js
@@ -0,0 +1,8 @@
+const csvUS = `NohKaLikai Falls;IN;IN;easy;25.27542;91.68595
+Kunchikal Falls;IN;IN;easy;13.69375;75.01913
+Barehipani Waterfall;IN;IN;easy;21.93288;86.38018
+Gj;IN;IN;easy;23.72134;68.18249
+Devprayag;IN;IN;easy;30.14528;78.59766
+TN;IN;IN;easy;8.07788;77.55578
+Afg;IN;IN;easy;37.05212;74.56856
+NER;IN;IN;easy;27.88023;97.37346`;

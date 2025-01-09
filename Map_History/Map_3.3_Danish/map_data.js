6c2d87d14ec00eff87887909743b03552a55ff7b
shubhmadhavan <madhavanshubh@gmail.com>
2025-01-09 23:42:43 +0530

    2025-01-09
    - Index Overhaul
    - added Map for Danes in History Section
    - Changed Map loading logic
    - Optimized Game page loading

---
+++ Map_History/Map_3.3_Danish/map_data.js
@@ -0,0 +1,17 @@
+const mapData = {
+"Tranquebar (1620)" : "POINT (79.85475 11.0286)",
+"Masulipatnam (1625)" : "POINT (81.13027 16.18089)",
+"Calicut (1752)" : "POINT (75.78041 11.25875)",
+"Serampore (1755)" : "POINT (88.34057 22.75052)"
+};
+
+
+
+/* 
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+remove comma for ending river*/

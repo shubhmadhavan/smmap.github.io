5790fe035bc09db0905f4875031146bedcb09222
shubhmadhavan <madhavanshubh@gmail.com>
2025-01-10 15:04:16 +0530

    2025-01-10
    - added Nuclear Power Plants, Ports, Airports
    - formatting fixes for index and game pages
    - bug fixed and performance improvements

---
+++ Map_Economy/Map_2_Ports/map_data.js
@@ -0,0 +1,39 @@
+const mapData = {
+"Mundra port" : "POINT (69.70435 22.73678)",
+"Porbandar Port" : "POINT (69.58867 21.62143)",
+"Veraval Port" : "POINT (70.36905 20.90239)",
+"Pipavav Port" : "POINT (71.48981 20.9263)",
+"Bhavnagar Port" : "POINT (72.23183 21.75958)",
+"Bahruch/ Dahej Port" : "POINT (72.55953 21.6615)",
+"Adani Hazira/ Surat Port" : "POINT (72.64467 21.11693)",
+"Vandhavan (Major) Port" : "POINT (72.67276 19.92923)",
+"J Nehru/ Nhava Sheva (Major) Port" : "POINT (72.95048 18.94975)",
+"Ratnagiri Jaigarh" : "POINT (73.312 16.99014)",
+"Panaji (Major) Port (Goa)" : "POINT (73.82781 15.50141)",
+"Mormugao Port" : "POINT (73.80551 15.40408)",
+"Mangalore" : "POINT (74.83195 12.86275)",
+"Cochin Port" : "POINT (76.26782 9.95463)",
+"Alappuzha Port" : "POINT (76.31867 9.49374)",
+"VOC/ Tuticorin (Major) Port" : "POINT (78.17907 8.75626)",
+"Nagapattinam Intl Port" : "POINT (79.84914 10.76297)",
+"Chennai Major Port" : "POINT (80.29205 13.08153)",
+"Kamarajar/ Ennore" : "POINT (80.3374 13.25925)",
+"(Adani) Kattupalli Port" : "POINT (80.34045 13.31233)",
+"Krishnapatnam Port" : "POINT (80.14081 14.25266)",
+"Machilipatnam Port" : "POINT (81.22081 16.22704)",
+"Kakinada / Coringa Port" : "POINT (82.26104 16.94561)",
+"Vizag (Major) Port" : "POINT (83.29273 17.68593)",
+"Paradip (Major) Port" : "POINT (86.69841 20.29864)",
+"Haldia (Major) Port" : "POINT (88.08904 22.04463)",
+"SP Mukherjee (Major) Port" : "POINT (88.25447 22.5055)"
+};
+
+
+/* 
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+remove comma for ending river*/
---
+++ Map_Economy/Map_5_Airports/map_data.js
@@ -0,0 +1,24 @@
+const mapData = {
+"Kushinagar Airport" : "POINT (83.89614 26.76934)",
+"Pakyong Airport" : "POINT (88.58769 27.22665)",
+"Donyi Polo / Itanagar Airport" : "POINT (93.6423 26.97184)",
+"Rajkot Airport" : "POINT (70.7793769 22.31047)",
+"Durgapur/ Kazi Nazrul Islam" : "POINT (87.23986 23.6178)",
+"Shirdi Intl" : "POINT (74.39406 19.69238)",
+"Sindhudurg Airport" : "POINT (73.55941 16.16683)",
+"Kannur Intl" : "POINT (75.54617 11.91715)",
+"Kalaburagi Airport" : "POINT (76.95337 17.3104)",
+"Kurnool/ Orvakal Airport" : "POINT (78.16154 15.71355)",
+"Mopa/ Manohar Intl Airport" : "POINT (73.86845 15.73181)",
+"Shivamogga Airport" : "POINT (75.59912 13.86027)"
+};
+
+
+/* 
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+remove comma for ending river*/

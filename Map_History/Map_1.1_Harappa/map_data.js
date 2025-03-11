71a7069e1c173219134b79cdcd59f11d8b0c29ea
shubhmadhavan <madhavanshubh@gmail.com>
2025-03-11 17:49:35 +0530

    2025-03-11
    ● Added following New Maps
    - Chalcolithic Sites (TN11, Upinder)
    - Harappa / Indus Valley Civilisation (NCERT Themes I + Upinder Singh)
    - 16 Later Vedic Mahajanapadas (Wikipedia)
    - Ashokan Rock Edicts & Pillars (NCERT Themes I)
    ● Changed Index css to accomodate for the same

---
+++ Map_History/Map_1.1_Harappa/map_data.js
@@ -0,0 +1,23 @@
+const mapData = {
+"Manda":"POINT (74.6971257 32.8924965)",
+"Harappa (Pak)":"POINT (72.8929 30.611015)",
+"Lakhmirwala (Mansa | PJ)":"POINT (75.3929523 29.8601656)",
+"Kalibangan (RS)":"POINT (74.1328046 29.4701189)",
+"Banawali (HR)":"POINT (75.4330161 29.3963693)",
+"Rakhigarhi (HR)":"POINT (76.1131495 29.2881189)",
+"Mitathal (HR)":"POINT (76.181272 28.8833835)",
+"Alamgirpur (UP)":"POINT (77.4917065 29.0089034)",
+"Ganweriwala (Pak)":"POINT (71.149988 28.5988908)",
+"Mohenjo-daro (Pak)":"POINT (68.1371795 27.3287231)",
+"Kot Diji (Sindh | Pak)":"POINT (68.7214593 27.3559022)",
+"Aamri (Sindh | Pak)":"POINT (68.0149609 26.1706885)",
+"Kot Bala or Balkot (Baluchistan | Pak)":"POINT (66.7762244 25.4007588)",
+"Sutkagan Dor (Enmost)":"POINT (61.9508521 25.4999183)",
+"Dholavira (Kachchh | GJ)":"POINT (70.2140444 23.8808877)",
+"Surkotada (Kachchh":"POINT (70.9174176 23.6112541)",
+"Nageshvar (Kachchh | GJ) ":"POINT (69.0910622 22.3173344)",
+"Somnath (GJ)":"POINT (70.3843721 20.9060022)",
+"Lothal (Sabarmati River | GJ)":"POINT (72.2493601 22.5228574)",
+"Rangpur (Bhadar River | GJ)":"POINT (72.7555249 23.4591987)",
+"Daimabad (Pravara River | MH)":"POINT (74.7012035 19.5107011)"
+}

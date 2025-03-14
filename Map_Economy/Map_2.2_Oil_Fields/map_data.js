6a9b1fa40ba8ff691860d2967139b064124890e5
shubhmadhavan <madhavanshubh@gmail.com>
2025-03-14 19:03:10 +0530

    2025-03-14
    ● added the following maps:
    - Oil Fields
    - Natural Gas
    ● updated the following:
    - NPP Map

---
+++ Map_Economy/Map_2.2_Oil_Fields/map_data.js
@@ -0,0 +1,21 @@
+const mapData = {
+"Mangla, Bhagyam, Aishwarya Oil-fields (Barmer Basin | RS)":"POINT (71.52123 25.95188)", 
+"Kalol (Gandhinagar | GJ)":"POINT (72.4929716 23.2418194)",
+"Ankleshwar (Bharuch | GJ)":"POINT (73.0151984 21.6264236)",
+"Bassein":"POINT (72.0651061 19.7724722)",
+"Mumbai High":"POINT (72.4338321 18.674367)",
+"Digboi (Tinsukia | AS)":"POINT (95.6286636 27.395032)",
+"Naharkatia (Dibrugarh | AS)":"POINT (95.325754 27.2862086)",
+"Moran (Dibrugarh | AS)":"POINT (94.9195154 27.175724)",
+"Sivasagar (AS)":"POINT (94.639424 26.989087)"
+};
+
+
+/* 
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+remove comma for ending river*/
--- Map_Economy/Map_3.1_NPP/map_data.js
+++ Map_Economy/Map_3.1_NPP/map_data.js
@@ -2,7 +2,7 @@ const mapData = {
 "Narora Atomic Power Station" 
 : "POINT (78.40971 28.15734)",
 "Rawatbhata Atomic Power Station" : "POINT (75.58841 24.93609)",
-"Kakrapar Atomic Power Station" : "POINT (82.97972 26.4036)",
+"Kakrapar Atomic Power Station (GJ)<br><span style='font-size:10px;'>(near Mandvi, Surat & Tapi river)</span>" : "POINT (73.40918 21.19843)",
 "Tarapur Atomic Power Station" : "POINT (72.6617 19.83209)",
 "Kaiga Atomic Power Station" : "POINT (74.42158 14.8564)",
 "Kalpakkam NPP" : "POINT (80.15681 12.52381)",

a587d2e0356f008dc8c15e4add390dc8bd374e99
shubhmadhavan <madhavanshubh@gmail.com>
2025-05-03 22:12:29 +0530

    2025-12-03 22-12-17
    • added Bhakti Saints

--- Map_Economy/Map_3.1_NPP/map_data.js
+++ Map_Economy/Map_3.1_NPP/map_data.js
@@ -1,7 +1,7 @@
 const mapData = {
 "Narora Atomic Power Station" : "POINT (78.40971 28.15734)",
 "Rawatbhata Atomic Power Station" : "POINT (75.58841 24.93609)",
-"Kakrapar Atomic Power Station (GJ)<br><span id='nl'>(near Mandvi, Surat & Tapi river)</span>" : "POINT (73.40918 21.19843)",
+"Kakrapar Atomic Power Station (GJ)<br><span class='nl'>(near Mandvi, Surat & Tapi river)</span>" : "POINT (73.40918 21.19843)",
 "Tarapur Atomic Power Station" : "POINT (72.6617 19.83209)",
 "Kaiga Atomic Power Station" : "POINT (74.42158 14.8564)",
 "Kalpakkam NPP" : "POINT (80.15681 12.52381)",

783fc6e8b901401ef24ee0a41c31e02ef8088e6e
shubhmadhavan <madhavanshubh@gmail.com>
2026-04-15 05:44:10 +0530

    2026-04-15 05-44-08

--- Map_History/Map_3.3_Danish/map_data.js
+++ Map_History/Map_3.3_Danish/map_data.js
@@ -1,13 +1,18 @@
 const mapData = {
-"Tranquebar (1620)" : "POINT (79.85475 11.0286)",
+"Fort Dansborg at Tranquebar (1620)<br><span class='nl'> Raghunatha Nayak, ruler of Thanjavur, allowed them to export pepper to Denmark & keep possession of town</span>" : "POINT (79.85475 11.0286)",
 "Masulipatnam (1625)" : "POINT (81.13027 16.18089)",
 "Calicut (1752)" : "POINT (75.78041 11.25875)",
 "Serampore (1755)" : "POINT (88.34057 22.75052)"
 };
 
+/*
 
-
-/* 
+<br><span class='nl'> </span>
+<br><span class='nl'> </span>
+<br><span class='nl'> </span>
+<br><span class='nl'> </span>
+<br><span class='nl'> </span>
+<br><span class='nl'> </span>
 "":"",
 "":"",
 "":"",

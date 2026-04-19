c692d47539c9b7610a4f269cb56edd0848760c71
shubhmadhavan <madhavanshubh@gmail.com>
2026-04-20 03:54:18 +0530

    2026-04-20 03-54-15

--- Map_Economy/Map_1.3_Bauxite/map_data.js
+++ Map_Economy/Map_1.3_Bauxite/map_data.js
@@ -1,12 +1,56 @@
 const mapData = {
-"Bauxite → extracting aluminium. also used in cement, chemicals & heat-resistance.":"",
-"Aluminium → electrical cables, transport (aircraft, cars), packaging (foil, cans), and construction (windows, doors) due to its light weight and corrosion resistance":"",    
+"Bauxite Bauxite forms by lateritization (sp. kind of chemical weathering) – intense tropical leaching + removal of silica & bases + long stability + residual concentration of aluminium oxides":"",
+"Bauxite → extracting aluminium. Used in cement, chemicals & heat-resistance.":"",
+"Aluminium → electrical cables, transport (aircraft), packaging (foil, cans) due to light weight & corrosion-resistance. Aliminium also used in SMALL quantities in Steel as deoxidiser to improve steel quality.":"",    
+"":"",
+
+"MP":"",
 "Katni (MP)":"POINT (80.3893814 23.8343441)",
+"Jabalpur":"POINT (79.9338798 23.1685786)",
+"Shahdol":"POINT (81.3568619 23.300232)",
+"Mandla":"POINT (80.3713531 22.5978545)",
+"Balaghat":"POINT (80.1838293 21.812876)",
 "Amarkantak (MP)":"POINT (81.7558745 22.6796427)",
-"Maikal Hills (CG)":"POINT (81.5 22.5)",
+
+
+"CG":"",
+"Maikal Hills (CG)":"POINT (81.6 22.5)",
 "Bilaspur (CG)":"POINT (82.1409152 22.0796545)",
-"Koraput (OD)":"POINT (82.7123327 18.813487)"
+"Raigarh":"POINT (83.3955055 21.8982979)",
+
+
+"JH":"",
+"Daltonganj (Mediningagar)":"POINT (84.0768462 24.0465121)",
+"Lohardaga":"POINT (84.6869722 23.4415776)",
+"Gumla":"POINT (84.5379455 23.0441295)",
+
+
+"OD":"",
+"Sundargarh":"POINT (84.037224 22.120021)",
+"Balangir":"POINT (83.4846069 20.7011108)",
+"Kalahandi":"POINT (85.0843782 20.1359584)",
+"Koraput (OD)":"POINT (82.7123327 18.813487)",
+
+
+"GJ":"",
+"Kachchh":"POINT (69.8597406 23.7337326)",
+"Jamnagar":"POINT (70.05773 22.4707019)",
+"Junagadh":"POINT (70.4579436 21.5222203)",
+"Amreli":"POINT (71.2203555 21.6015242)",
+"Sabarkantha":"POINT (72.99329689999999 23.8476704)",
+"Kheda":"POINT (72.686499 22.750154)",
+
+"MH":"",
+"Kolhapur":"POINT (74.2481772 16.706369)",
+"Satara":"POINT (74.005667 17.688671)",
+
+
+"TN":"", 
+"Salem":"POINT (78.1460142 11.664325)"
+
 };
+
+
 
 
 /* 
--- Map_Geography/Map_Reservoirs/map_data.js
+++ Map_Geography/Map_Reservoirs/map_data.js
@@ -1,53 +1,57 @@
 const mapData = {
 
 "Neelam River (trib of Jhelum), JK":"",
-	"Kishanganga HE Project (JK)":"POINT (74.7519341 34.6486313)",
+"Kishanganga HE Project (JK)":"POINT (74.7519341 34.6486313)",
+
 
 "Jhelum River":"",
-	"Uri Hydle Dam (JK)":"POINT (74.18118 34.14075)",
-	"Mangla Dam Lake (POJK)":"POINT (73.6509807 33.2113568)",
+"Uri Hydle Dam (JK)":"POINT (74.18118 34.14075)",
+"Mangla Dam Lake (POJK)":"POINT (73.6509807 33.2113568)",
 
 
 "Chenab River":"",
-	"Salal Dam (JK)":"POINT (74.81118 33.14308)",
-	"Baglihar Dam (JK)":"POINT (75.3511229 33.1545699)",
-	"Ratle Dam (JK)":"POINT (75.785494 33.1756706)",
-	"Dulhasti Dam (JK)":"POINT (75.798862 33.3685412)",
+"Salal Dam (JK)":"POINT (74.81118 33.14308)",
+"Baglihar Dam (JK)":"POINT (75.3511229 33.1545699)",
+"Ratle Dam (JK)":"POINT (75.785494 33.1756706)",
+"Dulhasti Dam (JK)":"POINT (75.798862 33.3685412)",
 
 
 "Ravi River":"",	
-	"Ranjit Sagar Dam (PJ-JK border)":"POINT (75.7659211 32.4596104)",
-	"Shahpur Kandi Dam (PJ-JK border)":"POINT (75.6885592 32.3923972)",
+"Ranjit Sagar Dam (PJ-JK border) <br><span class='nl'>Ravi – Ranjit, Shah </span>":"POINT (75.7659211 32.4596104)",
+"Shahpur Kandi Dam (PJ-JK border)":"POINT (75.6885592 32.3923972)",
+
 
 "Uhl River (trib of Beas), HP":"",	
-	"Shanan HE Project (HP)":"POINT (76.8612144 32.0024304)",
+"Shanan HE Project (HP)":"POINT (76.8612144 32.0024304)",
+
 
 "Beas River":"",	
-	"Maharana Pratap Sagar (HP)/<br> Pong Dam (HP)<span class='nl'>creates the man-made Ramsar site Pong Dam (HP)</span>":"POINT (76.0166561 31.9916099)",
+"Maharana Pratap Sagar (HP)/<br> Pong Dam (HP)<span class='nl'>creates the man-made Ramsar site Pong Dam (HP)</span>":"POINT (76.0166561 31.9916099)",
+
 
 "Satluj River":"",	
-	"Gobind Sagar Reservoir (HP)<br><span class='nl'>created by Bhakra Dam (HP)</span>":"POINT (76.4858919 31.4133005)",
-	"Bhakra Dam (HP)":"POINT (76.451946 31.4164701)",
-
-"Tons River, HP-UK border":"",	
-	"Kishau Dam (HP-UK border, proposed)":"POINT (77.70443333333334 30.749805555555557)",
-	"Ichari Dam (HP-UK border)":"POINT (77.791111 30.613611)",
-
-"Yamuna River, UK":"",	
-	"Lakhwar Dam":"POINT (77.948925 30.517983)",
-	"Hathni Kund":"POINT (77.583337 30.3145471)",
+"Gobind Sagar Reservoir (HP)<br><span class='nl'>created by Bhakra Dam (HP)</span>":"POINT (76.4858919 31.4133005)",
+"Bhakra Dam (HP)":"POINT (76.451946 31.4164701)",
 
 
-"Chambal River, RS":"",
-	"Jawahar Sagar Dam (RS)":"POINT (75.6789803 25.0410762)",
-	"Rana-Pratap Sagar (RS)":"POINT (75.5490475 24.7900733)",
-	
+"Tons River (HP-UK border)":"",	
+"Kishau Dam (HP-UK border, proposed)":"POINT (77.70443333333334 30.749805555555557)",
+"Ichari Dam (HP-UK border)":"POINT (77.791111 30.613611)",
 
-"Chambal River, MP":"",
-	"Gandhi Sagar (MP)":"POINT (75.5143623 24.4536135)",
+
+"Yamuna River":"",	
+"Lakhwar Dam (Dehradun, UK)":"POINT (77.948925 30.517983)",
+"Hathni Kund (Yamunanagar dis, HR)":"POINT (77.583337 30.3145471)",
+
+
+"Chambal River":"",
+"Jawahar Sagar Dam (RS)":"POINT (75.6789803 25.0410762)",
+"Rana-Pratap Sagar (RS)":"POINT (75.5490475 24.7900733)",
+"Gandhi Sagar (MP)":"POINT (75.5143623 24.4536135)",
+
 
 "Banas River, RS":"",
-	"Bisalpur Reservoir":"POINT (75.3737003 25.856263)",
+	"Bisalpur Reservoir (Tonk, RS)":"POINT (75.3737003 25.856263)",
 	"Nandsamand Dam":"POINT (73.7747077 24.9274606)",
 
 
@@ -58,157 +62,146 @@ const mapData = {
 	"Madhikeda Reservoir":"POINT (77.8739264 25.5123334)",
 
 "Betwa":"",
-	"Sukma Dugma Reservoir (Jhansi, UP)":"POINT (78.5342696 25.1792106)",
-	"Mata Tila Dam Reservoir (Lalitpur, UP)":"POINT (78.3347613 25.0632176)",
-
-"Betwa":"",
-	"Rajghat Reservoir (UP-MP)<br><span class='nl'>Rajghat is an Inter-State Dam, Lalitpur, UP & Ashoknagar, MP</span> ":"POINT (78.263272 24.681173)",
+"Sukma Dugma Reservoir (Jhansi, UP)":"POINT (78.5342696 25.1792106)",
+"Mata Tila Dam Reservoir (Lalitpur, UP)":"POINT (78.3347613 25.0632176)",
+"Rajghat Reservoir (UP-MP)<br><span class='nl'>Rajghat is an Inter-State Dam, Lalitpur, UP & Ashoknagar, MP</span> ":"POINT (78.263272 24.681173)",
 
 "Virma (trib of Betwa)":"",
-	"Swami Brahma Nand Dam (UP)":"POINT (79.6823402 25.5498202)",
+"Swami Brahma Nand Dam (UP)":"POINT (79.6823402 25.5498202)",
 
 "Dhasan (trib of Betwa)":"",
-	"Sujara Band (MP)":"POINT (79.1003199 24.630049)",
-
+"Sujara Band (MP)":"POINT (79.1003199 24.630049)",
 
 
 "Bhagirathi":"",
-	"Tehri Dam (UK)":"POINT (78.4706599 30.4060548)",
+"Tehri Dam (UK)":"POINT (78.4706599 30.4060548)",
 
 "Ganga River":"",	
-	"Narora Dam (UP)":"POINT (78.39193 28.19817)",
+"Narora Dam (UP)":"POINT (78.39193 28.19817)",
 
 "Son":"",
-	"Ban Sagar Lake (MP)":"POINT (81.1161596 24.1128556)",
+"Ban Sagar Lake (MP)":"POINT (81.1161596 24.1128556)",
 
 "Rihand (trib of Son)":"",
-	"Govind Ballabh Pant Sagar/ Rihand Dam (MP)":"POINT (82.8297062 24.10856)",
+"Govind Ballabh Pant Sagar/ Rihand Dam (MP)":"POINT (82.8297062 24.10856)",
 
-"Manjushree (trib of Hooghly)":"",
-	"Massanjore Dam (JH)":"POINT (87.284051 24.1557136)",
-
-"JH":"",
-	"Konar Reservoir":"POINT (85.7516353 23.9444647)",
+"Mayurakshi (trib of Hooghly)":"",
+"Massanjore Dam (JH)":"POINT (87.284051 24.1557136)",
 
 
 "Damodar (trib of Hooghly), JH":"",
-	"Tenughat Reservoir":"POINT (85.7973052 23.7297787)",
+"Tenughat Reservoir":"POINT (85.7973052 23.7297787)",
 
 "Barakar (trib of Damodar), JH":"",
-	"Tilaiya Dam":"POINT (85.5215529 24.3243035)",
-	"Maithon":"POINT (86.8152079 23.8584476)",
+"Tilaiya Dam":"POINT (85.5215529 24.3243035)",
+"Maithon":"POINT (86.8152079 23.8584476)",
 
-"Swarnarekha River, JH":"",
-	"Getalsud Dam<br><span class='nl'>Getalsud (Swarnarekha River) not to be conf. w. Gersoppa (Shravathi River, KN)</span>":"POINT (85.5346714 23.4554267)",
-	"Hundru Waterfall":"POINT (85.6667986 23.4508392)",
-	"Chandil Reservoir":"POINT (86.0150167 23.0316844)",
 
-"Teesta, SK":"",
-	"Chungthang Dam":"POINT (88.6537239 27.5987441)",
+"JH":"",
+"Konar Reservoir":"POINT (85.7516353 23.9444647)",
 
-"Umiam, MG":"",
-	"Umiam/ Barapani Lake<br><span class='nl'>locally k.a Dam Sait</span>":"POINT (91.8928297 25.6654174)",
+"Swarnarekha River":"",
+	"Getalsud Dam (JH)<br><span class='nl'>Getalsud (Swarnarekha River) not to be conf. w. Gersoppa (Shravathi River, KN)</span>":"POINT (85.5346714 23.4554267)",
+	"Hundru Waterfall (JH)":"POINT (85.6667986 23.4508392)",
+	"Chandil Reservoir (JH)":"POINT (86.0150167 23.0316844)",
 
-"Sankh River, OD":"",
-	"Mandira Dam":"POINT (84.6528506 22.2713869)",
+"Sankh River":"",
+"Mandira Dam (OD)":"POINT (84.6528506 22.2713869)",
 
-"Brahmani River, OD":"",
-	"Rengali Reservoir":"POINT (84.9589402 21.3471019)",
+"Brahmani River":"",
+	"Rengali Reservoir (OD)":"POINT (84.9589402 21.3471019)",
 
 "Mahanadi, CG":"",
-	"Dudhwana Dam":"POINT (81.7745703 20.3086306)",
-	"Ganrel Reservoir":"POINT (81.5384444 20.5918119)",
+"Dudhwana Dam (CG)":"POINT (81.7745703 20.3086306)",
+"Ganrel Reservoir (CG)":"POINT (81.5384444 20.5918119)",
 
-"Mahanadi, OD":"",
-	"Hirakud Dam/ Reservoir":"POINT (83.7749867 21.6236917)",
 
-"Pairi, CG":"",
+"Hirakud Dam (OD)":"POINT (83.7749867 21.6236917)",
+
+"Pairi River":"",
 	"Saundur Dam":"POINT (82.1029349 20.2221791)",
 
 
 "CG":"",
 	"Balar Reservoir":"POINT (82.498539 21.5236469)",
 
-"Jonk, OD":"",
-	"Patora Dam":"POINT (82.4557589 20.7162745)",
+"Jonk River":"",
+	"Patora Dam (OD)":"POINT (82.4557589 20.7162745)",
 
-"Hasdeo, CG":"",
-	"Hasdeo Reservoir":"POINT (82.6507276 22.6670588)",
+"Hasdeo":"",
+"Hasdeo Reservoir (CG)":"POINT (82.6507276 22.6670588)",
 
-"Godavari, MH":"",
-	"Gangapur Bandh Sagar":"POINT (73.6638592 20.0303617)",
-	"Nath Sagar":"POINT (75.2681086 19.5028712)",
-	"Sriram Sagar Dam (TG)":"POINT (78.2050213 18.9969425)",
-
-
-"Pravara, MH":"",
-	"Nilwande Dam <br><span class='nl'>Upper Pravara Dam":"POINT (73.9014 19.54617)", 
-
-"Purna, MH":"",
-	"Khadakpurna Dam":"POINT (76.1156826 20.1145641)",
-	"Yeldari Dam":"POINT (76.7163362 19.76659)",
-
-"Pench, MH":"",
-	"Totladoh Dam (MH)":"POINT (79.2317032 21.5790799)",
-
-"Indravati, OD":"",
-	"Indravati Power Station":"POINT (82.8420304 19.2889818)",
+"Godavari":"",
+"Gangapur Bandh Sagar (MH)":"POINT (73.6638592 20.0303617)",
+"Nath Sagar (MH)":"POINT (75.2681086 19.5028712)",
+"Sriram Sagar Dam (TG)":"POINT (78.2050213 18.9969425)",
 
 
+"Pravar (trib of Godavari)":"",
+"Nilwande Dam <br><span class='nl'>Upper Pravara Dam":"POINT (73.9014 19.54617)", 
 
-"Silleru, OD":"",	
-	"Jolaput Reservoir":"POINT (82.5572162 18.4418563)",
-	"Balimela Reservoir":"POINT (82.2087787 18.2477451)",
+"Purna (trib of Godavari)":"",
+"Khadakpurna Dam (MH)":"POINT (76.1156826 20.1145641)",
+"Yeldari Dam (MH)":"POINT (76.7163362 19.76659)",
+
+"Pench (trib of Wainganga, itself trib of Godavari)":"",
+"Totladoh Dam (MH)":"POINT (79.2317032 21.5790799)",
+
+"Indravati":"",
+"Indravati Power Station (OD)":"POINT (82.8420304 19.2889818)",
 
 
-
-"Krishna, MH":"",
-	"Almatti Reservoir<br><span class='nl'>aka LBS Dam</span>":"POINT (75.9055695 16.3684643)",
-	"Jurala Dam":"POINT (77.6969432 16.3257043)",
-	"Srisailam Sagar":"POINT (78.8877078 16.0908526)",
-	"Nagarjuna Sagar":"POINT (79.2531772 16.5533433)",
-	"Pulichintala Dam":"POINT (80.0621581 16.7706243)",
+"Silleru":"",	
+"Jolaput Reservoir (OD)":"POINT (82.5572162 18.4418563)",
+"Balimela Reservoir (OD)":"POINT (82.2087787 18.2477451)",
 
 
-"Bhima, MH":"",
-	"Ujani Dam":"POINT (75.0961549 18.1364287)",
+"Krishna":"",
+"Almatti Reservoir (KN)<br><span class='nl'>aka Lal Bahadur Shastri Dam</span>":"POINT (75.9055695 16.3684643)",
+"Jurala Dam (TG)":"POINT (77.6969432 16.3257043)",
+"Srisailam Sagar (TG-AP border)":"POINT (78.8877078 16.0908526)",
+"Nagarjuna Sagar (TG-AP border)":"POINT (79.2531772 16.5533433)",
+"Pulichintala Dam (AP)":"POINT (80.0621581 16.7706243)",
 
-"Koyna, MH":"",
-	"Koyna Dam":"POINT (73.7513597 17.402477)",
+
+"Bhima (trib of Krishna)":"",
+"Ujani Dam (Solapur, MH)":"POINT (75.0961549 18.1364287)",
+
+"Koyna (right trib of Krishna River)":"",
+"Koyna Dam (MH)":"POINT (73.7513i597 17.402477)",
 
 "Ghatprabha River":"",
-	"Phatakwadi Lake (MH)":"POINT (74.0626456 15.9412602)",
-	"Hidkal Dam (KN)":"POINT (74.642605 16.1462048)",
+"Phatakwadi Lake (MH)":"POINT (74.0626456 15.9412602)",
+"Hidkal Dam (KN)":"POINT (74.642605 16.1462048)",
 
 
 "Bhadra (trib of Tungabhadra), MH":"",
-	"Bhadra Res":"POINT (75.6301533 13.6990153)",
+"Bhadra Res":"POINT (75.6301533 13.6990153)",
 
-"Musi, TG":"",
-	"Osman Sagar":"POINT (78.2973917 17.3813991)",
-	"Himayat Sagar":"POINT (78.3607855 17.3295125)",
-	"Hussain Sagar":"POINT (78.478319 17.4255822)",
+"Musi":"",
+"Osman Sagar (TG)":"POINT (78.2973917 17.3813991)",
+"Himayat Sagar (TG)":"POINT (78.3607855 17.3295125)",
+"Hussain Sagar (TG)":"POINT (78.478319 17.4255822)",
 
-"Penna, AP":"",
-	"Gandikota Dam":"POINT (78.2862866 14.8149269)",
-	"Somasila Dam":"POINT (79.3098662 14.4958905)",
-	"Kanigiri Res <br><span class='nl'>Kanigiri Reservoir receives water from Sangam Barrage on Penna River</span>":"POINT (79.8430506 14.595211)",
+"Penna":"",
+	"Gandikota Dam (AP)":"POINT (78.2862866 14.8149269)",
+	"Somasila Dam (AP)":"POINT (79.3098662 14.4958905)",
+	"Kanigiri Res (AP)<br><span class='nl'>Kanigiri Reservoir receives water from Sangam Barrage on Penna River</span>":"POINT (79.8430506 14.595211)",
 
 "AP":"",
 	"Kandaleru Reservoir":"POINT (79.5940544 14.347401)",
 
-"Ponnaiyar, TN":"",
-	"Krishnagiri Dam":"POINT (78.1696715 12.4973078)",
+"Ponnaiyar":"",
+	"Krishnagiri Dam (Krishnagiri, TN)<br><span class='nl'>Ponnaiyar river passes through Krishnagiri dis</span> ":"POINT (78.1696715 12.4973078)",
 
-"Kaveri, KN":"",
+"Kaveri":"",
 	"Krishnaraja Sagara Res (KN)":"POINT (76.4967919 12.457095)",
 	"Mekedatu Dam (KN)":"POINT (77.4458151 12.2576017)",
 	
-"Kaveri, TN":"",
 	"Mettur Dam Stanley Res (TN)":"POINT (77.8334992 11.9024462)",
 
-"Hemavati, KN":"",
-	"Gorur Hemavati Res":"POINT (76.0365749 12.8206986)",
+"Hemavati":"",
+	"Gorur Hemavati Res (KN)":"POINT (76.0365749 12.8206986)",
 
 "TN":"",
 	"Pykara Lake":"POINT (76.5899997 11.4370536)",
@@ -219,8 +212,8 @@ const mapData = {
 "Nayyali":"",
 	"Singanallur":"POINT (77.0219106 10.9880618)",
 
-"Kodaganar, TN":"",
-	"Alagapuri Dam":"POINT (77.9690203 10.584302)",
+"Kodaganar":"",
+"Alagapuri Dam (TN)":"POINT (77.9690203 10.584302)",
 
 "Thamarabarani, TN":"",
 	"Karayar Dam":"POINT (77.310444 8.651334)",
@@ -249,40 +242,49 @@ const mapData = {
 "Zuari, GA":"",
 	"Salaulim Dam":"POINT (74.1776131 15.197038)",
 
-"Kalindi, KN":"",
-	"Supa Dam Res":"POINT (74.4878124 15.2667415)",
-	"Bommanahali Res":"POINT (74.7029042 15.1978392)",
-	"Tattihalla Dam":"POINT (74.7890131 15.1279931)",
+"Kalindi":"",
+	"Supa Dam Res (KN)":"POINT (74.4878124 15.2667415)",
+	"Bommanahali Res (KN)":"POINT (74.7029042 15.1978392)",
+	"Tattihalla Dam (KN)":"POINT (74.7890131 15.1279931)",
 
-"Shravathi, KN":"",
-	"Gersoppa Res":"POINT (74.700503 14.2615121)",
-	"Mahatma Gandhi HE Plant":"POINT (74.7976709 14.2280797)",
-	"Linganamakki Dam":"POINT (74.8456054 14.1764253)",
+"Shravathi":"",
+	"Gersoppa Res (KN)":"POINT (74.700503 14.2615121)",
+	"Mahatma Gandhi HE Plant (KN)":"POINT (74.7976709 14.2280797)",
+	"Linganamakki Dam (KN)":"POINT (74.8456054 14.1764253)",
 
-"Bharatphuza, TN":"",
-	"Thoomurthy Dam":"POINT (77.1575358 10.4837169)",
-"Bharatphuza, KL":"",
-	"Vankalakayam Dam":"POINT (76.8273807 10.6629249)",
+"Bharatphuza":"",
+"Thoomurthy Dam (TN)":"POINT (77.1575358 10.4837169)",
+"Vankalakayam Dam (KL)":"POINT (76.8273807 10.6629249)",
 
-"Uppar, TN":"",
-	"Aliyar Dam":"POINT (76.9671864 10.4823747)",
 
-"Gayathri, KL":"",
-	"Meenkara Dam":"POINT (76.8022488 10.609581)",
+"Uppar (trib of Bharatphuza)":"",
+	"Aliyar Dam (TN)":"POINT (76.9671864 10.4823747)",
 
-"Periyar, KL":"",
-	"Idukki Dam":"POINT (76.9772021 9.8429427)",
-	"Mullaperiyar Dam":"POINT (77.1491414 9.525965)",
+"Gayathri":"",
+	"Meenkara Dam (KL)":"POINT (76.8022488 10.609581)",
 
-"Chalakudy, KL":"",
+"Periyar":"",
+	"Idukki Dam (KL)":"POINT (76.9772021 9.8429427)",
+	"Mullaperiyar Dam (KL)":"POINT (77.1491414 9.525965)",
+
+"Chalakudy (trib of Periyar)":"",
 	"Shoyalar Res":"POINT (76.7686944 10.3116351)",
 
 "Pamba, KL":"",
 	"Sabarigiri/ Pamba Dam<br><span class='nl'>on Pamba River, KL</span>":"POINT (77.159800 9.390600)",
 
 "Kalladar, KL":"",
-	"Kallara Meenmutty WF<br><span class='nl'>despite its name, its NOT on Kallada River – it is on Vamanapuram River</span>":"POINT (77.0857212 8.9470509)"
-	};
+"Kallara Meenmutty WF<br><span class='nl'>despite its name, its NOT on Kallada River – it is on Vamanapuram River</span>":"POINT (77.0857212 8.9470509)",
+	
+
+"Teesta, SK":"",
+"Chungthang Dam":"POINT (88.6537239 27.5987441)",
+
+"Umiam, MG":"",
+"Umiam/ Barapani Lake<br><span class='nl'>locally k.a Dam Sait</span>":"POINT (91.8928297 25.6654174)"
+
+
+};
 
 
 

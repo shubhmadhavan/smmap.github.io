e0bfc708417894bcebf6d2175d004cdb56441b0e
shubhmadhavan <madhavanshubh@gmail.com>
2026-05-09 13:21:10 +0530

    2026-05-09 13-21-09

--- Map_Economy/Map_1.2_Manganese/map_data.js
+++ Map_Economy/Map_1.2_Manganese/map_data.js
@@ -1,11 +1,14 @@
 const mapData = {
 "Manganese is used in steelmaking (improving strength), toughness, wear resistance, Li-ion battery (MnO₂ cathode), aluminium alloys":"",
-"Manganese States: MP, MH and OD are top manganese-producing states in India, contributing to over 70% of national output":"",
 "Manganese largest producers – South Africa & China":"",
+"Manganese States: MP, MH and OD are top manganese-producing states in India, contributing to over 70% of national output":"",
+"Many manganese deposits in India, esp in Dharwar and Gondwana are due to metamorphosis of sedimentary rocks":"",
+"Manganese nodules (with ~24% Mn) occur on Ocean floors.":"",
 
+"Chhindwara (MP)":"POINT (78.9382293 22.052876)",
 "Balaghat (MP)":"POINT (80.1838293 21.812876)",
-"Nagpur (MH)":"POINT (79.0881546 21.1458004)",
-"Bandhara (MH)":"POINT (78.7828613 21.1754754)",
+"Bandhara (MH)":"POINT (79.0881546 21.1458004)",
+"Nagpur (MH)":"POINT (78.7828613 21.1754754)",
 "Sundargarh (OD)":"POINT (84.037224 22.120021)",
 "Keonjhar (OD)":"POINT (85.605525 21.6393113)",
 "Belagavi":"POINT (74.4976741 15.8496953)",

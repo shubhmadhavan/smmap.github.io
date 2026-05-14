a6b438c8fddb6391b6861ff618eb9a84c39a21ed
shubhmadhavan <madhavanshubh@gmail.com>
2026-05-14 21:24:29 +0530

    2026-05-14 21-24-27

--- Map_Geography/Map_Waterfalls/map_data.js
+++ Map_Geography/Map_Waterfalls/map_data.js
@@ -1,8 +1,19 @@
 const mapData = {
+"Waterfalls – a geological formation where flowing water rapidly drops in elevation.":"",
+"Types – Plunge (vertical drop, loses contact with cliff), Horsefall (retains contact), Cascade (series of softer steps with a gentle slope, water 'tumbling down'), Tiered WF (distinct, easily distinguishable staircase-like steps, sometimes each splits into separate, smaller pools) ":"",
+"Angels Falls (Salto Angel, 'plunge' WF) in Venezuela (Canaima NP in Guiana Highlands) is the highest uninterrupted waterfall (900+m), followed by Tugela Falls (South Africa) & Tres Hermanas (Peru)":"",
+"":"",
+"The tallest (overall) WF is the Kunchikal Falls (Shimoga, KN) which is a cascade WF, followed by Barehipani in OD ('tiered' waterfall)":"",
+"The tallest plunge WF in India is Nohkalikai Falls (300m+) in MG":"",
+" ":"",
 
 "UK":"",
 "Kempty Waterfall":"POINT (78.0371964 30.4879258)",
 "Sahastradhara<br> <span class='nl'>NOT on Ganges. Sahastradhara is on Baldi River. Ganges → Sooswa → Song → Baldi River (Sahasradhara Falls)<span>":"POINT (78.1293615 30.3883919)",
+
+"MG":"",
+"Elephant Falls":"POINT (91.8225513 25.5375374)",
+"Nohkalikai WF etc<br><span class='nl'>Nohkalikai WF is 3rd highest 'overall' and 'highest plunge' WF; Other seasonal WFs loc in vicinity of Nohkalikai WF are – Nohsngithiang WF (4th highest overall – tiered) and Kynrem WF; Nohsngithiang WF (4th highest WF) is aka Mawsmai WF ":"POINT (91.732537 25.2389306)",
 
 "RS":"",
 "Chuliya Waterfall<br><span class='nl'>on ChambaL (chuLiya); NOT to be conf. w. Chachai on Bihad (trib of Tamsa) ":"POINT (75.5711968 24.9305361)",
@@ -10,41 +21,36 @@ const mapData = {
 "MP":"",
 "Keoti Falls<br><span class='nl'> Rewa dis; on Mahana, trib of Tamsa":"POINT (81.4529612 24.8164113)",
 "Chachai Waterfall<br><span class='nl'> Rewa dis; on Bihad, a trib of Tamsa; east of the Chahai, the discovery of a prehistoric chert arrowhead at Nyagurhee village was made by Le Mesurier (RW engineer) in 1856":"POINT (81.30249 24.791860)",
-
-"MP":"",
 "Dhuandhar Waterfall<br><span class='nl'> Jabalpur dis ":"POINT (79.8075411 23.1262738)",
 "Kapildhara Waterfall<br><span class='nl'> on Narmada (CORRECT RIVER MAP)":"POINT (80.7055688 22.7008773)",
+
+"CG":"",
+"Tirathgarh Waterfall":"POINT (81.8651088 18.9139499)",
+
+
 
 "JH":"",
 "Rajrappa Waterfall<br><span class='nl'> Ramgarh dis, JH; at confluence of Damodar & Bhairavi":"POINT (85.7080133 23.6415377)",
 "Hundru Falls<br><span class='nl'>Ranchi dis; on Subarnarekha River":"POINT (85.6667986 23.4508392)",
 "Jonha Falls <br><span class='nl'>Ranchi dis; on Raru, a trib of Subarnarekha":"POINT (85.6102055 23.3423069)",
 
+
 "OD":"",
-"Barehipani Waterfall<br><span class='nl'> 2nd highest 'overall' – Barehpani in OD is tiered WF; recall that the tallest 'overall' WF is also tiered → Kunchikal on Varahi River, KN ":"POINT (86.3801873 21.9328896)",
-
-"MG":"",
-"Elephant Falls":"POINT (91.8225513 25.5375374)",
-"Nohkalikai WF etc<br><span class='nl'>Nohkalikai WF is 3rd highest 'overall' and 'highest plunge' WF; Other seasonal WFs loc in vicinity of Nohkalikai WF are – Nohsngithiang WF (4th highest overall – tiered) and Kynrem WF; Nohsngithiang WF (4th highest WF) is aka Mawsmai WF ":"POINT (91.732537 25.2389306)",
-
-"CG":"",
-"Tirathgarh Waterfall":"POINT (81.8651088 18.9139499)",
-
-"OD (Silleru River)":"",
+"Barehipani Waterfall<br><span class='nl'>Barehipani Falls lies on Budhabalanga River in OD; Barehipani is the 2nd highest WF in India; Barehpani in OD is tiered WF; recall that the tallest 'overall' WF is also tiered → Kunchikal on Varahi River, KN ":"POINT (86.3801873 21.9328896)",
 "Duduma Waterfalls<br><span class='nl'>Dudma on Silleru River</span> ":"POINT (82.4531033 18.519303)",
 
+
 "MH":"",
-"Vajrai Waterfall<br><span class='nl'> NOT on Koyna but on Urmodi River; there is also Urmodi Reservoir on the same":"POINT (73.80947 17.69711)",
+"Vajrai WF<br><span class='nl'> Vajrai WF lies on Urmodi River (a trib of Krishna) and NOT on Koyna; there is also Urmodi Reservoir on the same":"POINT (73.80947 17.69711)",
+
+"GA":"",
+"Dudhsagar Falls (GA)<br><span class='nl'> Dudhsagar WF lies on Mandovi River in Goa; It is the 5th highest waterfall – cascade WF":"POINT (74.3143073 15.3144375)",
 
 "KN":"",
 "Gokak Falls<br><span class='nl'> on Ghatprabha":"POINT (74.7775938 16.1878993)",
 "Jog/ Gersoppa Falls<br><span class='nl'>Jog Falls on Shravathi River</span> ":"POINT (74.8138496 14.2293266)",
-
-
-"GA":"",
-"Dudhsagar Falls (GA)<br><span class='nl'> Goa; on Mandovi River – 5th highest waterfall – segmented WF":"POINT (74.3143073 15.3144375)",
-"Kunchikal Falls<br><span class='nl'>highest 'overall' waterfall of India – Kunchikal is a tiered cascade, NOT a single-drop (plunge) waterfall; Varahi River, which also has Mani Dam":"POINT (75.0191385 13.6937514)",
-"Barkana falls" :"POINT (75.0935483 13.4881474)",
+"Kunchikal Falls<br><span class='nl'>Kunchikal WF lies on Varahi River & is located in Shimoga dis, KN; It is the tallest 'overall' WF (400m+) of India; Kunchikal is a cascade (NOT a single-drop plunge) WF; Varahi River, which also has Mani Dam":"POINT (75.0191385 13.6937514)",
+"Barkana falls<br><span class='nl'>Barkana Falls lies on Sita River, a minor west-flowing river of Karnataka</span> " :"POINT (75.0935483 13.4881474)",
 "Shivanasamudra Falls<br><span class='nl'>Shivanasamudra is on Kaveri River</span>":"POINT (77.1680322 12.2944508)",
 
 "TN":"",
@@ -54,10 +60,10 @@ const mapData = {
 
 
 "KL":"",
-"Soochipara Waterfalls":"POINT (76.1637859 11.5115356)",
-"Vazhachal Waterfalls <br><span class='nl'>lies on Chalakudy River":"POINT (76.5925893 10.3014146)",
+"Soochipara WF<br><span class='nl'>Soochipara Falls or Sentinel Rock Falls lies on Chaliyar River; Note that Chaliyar River (Northern Kerala) is NOT to be confused with Chalakudy (trib of Periyar) on which Vazhachal and Athirappilly Falls lie; Soochipara Falls is in Wayanad dis, KL</span> ":"POINT (76.1637859 11.5115356)",
+"Vazhachal WF <br><span class='nl'>lies on Chalakudy River":"POINT (76.5925893 10.3014146)",
 "Athirappilly Falls <br><span class='nl'>lies on Chalakudy River;largest waterfall of KL; 'Niagara of S Ind'":"POINT (76.5697643 10.2851072)",
-"Kallar (Meenmutty) Waterfalls (Thiruvanthapuram, KL)":"POINT (77.1452266 8.7122176)"
+"Meenmutty Falls <br><span class='nl'>Meenmutty or Kallar Falls is near the Banasura Sagar Dam and is linked to Banasura Hills in Thiruvanthapuram, KL; Note that despite being also called Kallar Falls, it is MUCH further south from Kallada River and does not lie on it</span>":"POINT (77.1452266 8.7122176)"
 	};
 
 
